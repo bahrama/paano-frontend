42a03df1df937d3958dbb308eb06ac7303585386
Ali The Boss <alialikhahasl@gmail.com>
2023-04-02 13:25:17 +0430

    fnt11:menu changed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -19,7 +19,7 @@ const Header = () =>{
                       <div className="mr-7">
                         <ul className="flex gap-7 font-vazir">
                           <li className="nav-item-active"><Link href={"/"}>صفحه اصلی</Link></li>
-                          <li classNa me="nav-item"><Link onClick={handleProductMenuExpand} href={"#"}>محصولات</Link>
+                          <li class="nav-item"><Link onClick={handleProductMenuExpand} href={"#"}>محصولات</Link>
                               {productMenuExpand ?
                                   <div className="bg-white absolute top-14 border rounded-lg py-2 px-2 transition duration-300 ease-in-out">
                                       <ul className="space-y-3">
